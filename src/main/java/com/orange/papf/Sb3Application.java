adc1ab21cd3ef5fb1303c58c1c8784c5a974baeb
Nicolas LOYAU <nicolas.loyau@icloud.com>
2023-01-09 10:43:45 +0100

    first commit

---
+++ src/main/java/com/orange/papf/Sb3Application.java
@@ -0,0 +1,13 @@
+package com.orange.papf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3Application.class, args);
+	}
+
+}
